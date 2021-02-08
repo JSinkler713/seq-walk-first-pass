380d014b444010f0cfff7fa61c579f6bb7f1a405
James Sinkler <jamessinkler713@gmail.com>
2021-02-07 20:43:25 -0800

    delete button, and route for delete pet set up

--- controllers/animalsController.js
+++ controllers/animalsController.js
@@ -3,5 +3,12 @@ const router = express.Router()
 // bring in db
 const db = require('../models')
 
+router.delete('/:id', (req, res)=> {
+  db.pet.destroy({ where: { id: req.params.id } })
+  .then((deletedSuccess)=> {
+    res.redirect('/users')
+  })
+})
+
 
 module.exports = router
